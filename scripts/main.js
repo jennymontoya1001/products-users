75e7121844f30b5e789d13706d7e854fb7ccc3bb
Jenny Alexandra Montoya Cortés <30633488+jennymontoya1001@users.noreply.github.com>
2021-11-24 18:01:11 -0500

    ejercicio completo

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+import getData from "./getData.js";
+import {showData} from './showData.js'
+
+const endpoint = 'http://localhost:4000/products/';
+
+const element = document.querySelector('.list-group');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const data = getData(endpoint);
+    showData(data,element);
+
+})
+
+element.addEventListener('click', async(e) => {
+
+    const btnDetail = e.target.classList.contains('btn-outline-dark');
+    const id = e.target.id;
+
+    if(btnDetail){
+         const lista = await getData(endpoint);
+         const objeto = lista.find(list => list.id === Number(id))
+         localStorage.setItem("Detail",JSON.stringify(objeto));
+         window.location.href = "detail.html"
+    }
+
+    
+
+})
---
+++ scripts/showData.js
@@ -0,0 +1,18 @@
+
+
+
+export const showData = async(list,element) => {
+    const products = await list;
+    products.forEach(prod => {
+        const {id,name,img} = prod;
+        element.innerHTML += `
+        <li class="card" style="width: 18rem;">
+        <div class="card-body" >
+        <img src=${img} class="card-img-top" alt="...">
+          <h5 class="card-title">${name}</h5>
+          <a href="#" id=${id} class="btn btn-outline-dark " style="background-color: #db7093;">Detail</a>
+        </div>
+      </li>
+        `
+    })
+}
